913f2563736405c74dbf5e74dd412d41402ae1a9
sudhin99 <71205830+sudhin99@users.noreply.github.com>
2026-07-11 20:45:58 +0530

    Create silver_feedback.sql

---
+++ SQLScripts/silver/silver_feedback.sql
@@ -0,0 +1,17 @@
+USE RELIANT_DWH_SILVER;
+
+DROP TABLE IF EXISTS SILVER_FEEDBACK;
+
+CREATE TABLE SILVER_FEEDBACK (
+    feedback_id   INT,
+    customer_id   INT,
+    customer_name VARCHAR(200),
+    store_id      INT,
+    store_name    VARCHAR(200),
+    rating        INT,
+    comment       TEXT,
+    channel       VARCHAR(50),
+    feedback_date DATE,
+    created_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at    TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
